a88fa5fc21f6c8d2733192fff2e21728b2068021
Karthi-DS <kartheesvaranronaldo@gmail.com>
2025-11-27 20:45:09 +0530

    update intellisense

--- trie.c
+++ trie.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-
+#include <conio.h>
 
 #define NUM_CHAR 256
 
 
-typedef struct trienode{
-    struct trienode* children[NUM_CHAR];
+typedef struct trie{
+    struct trie* children[NUM_CHAR];
     bool terminal;
-} trienode;
+} trie;
 
 
-trienode* createNode(){
-    trienode* newNode = malloc(sizeof(trienode));
+trie* createNode(){
+    trie* newNode = malloc(sizeof(trie));
     //setting null
     for(int i=0;i<NUM_CHAR;i++){
         newNode->children[i] = NULL;
@@ -23,14 +23,14 @@ trienode* createNode(){
     return newNode;
 }
 
-bool insertNode(trienode** root,char* str){
+bool insertNode(trie** root,char* str){
 
     if(*root == NULL){
         *root = createNode();
     }
 
     unsigned char* text = (unsigned char*)str;
-    trienode* tmp = *root;
+    trie* tmp = *root;
     int len = strlen(str);
 
     for(int i=0;i<len;i++){
@@ -48,20 +48,9 @@ bool insertNode(trienode** root,char* str){
     }
 }
 
-bool searchStr(trienode* root, char* str){
-    trienode* tmp = root;
-    int len = strlen(str);
 
-    for(int i=0;i<len;i++){
-        if(tmp->children[str[i]]==NULL){
-            return false;
-        }
-        tmp = tmp->children[str[i]];
-    }
-    return tmp->terminal;
-}
 
-bool hasChildren(trienode* node) {
+bool hasChildren(trie* node) {
     for (int i = 0; i < NUM_CHAR; i++) {
         if (node->children[i] != NULL)
             return true;
@@ -69,7 +58,7 @@ bool hasChildren(trienode* node) {
     return false;
 }
 
-bool deleteTrieUtil(trienode* node, char* str, int depth, bool* wordDeleted) {
+bool deleteTrieUtil(trie* node, char* str, int depth, bool* wordDeleted) {
     if (node == NULL)
         return false;
 
@@ -97,7 +86,7 @@ bool deleteTrieUtil(trienode* node, char* str, int depth, bool* wordDeleted) {
 }
 
 
-void deleteWord(trienode** root, char* str) {
+void deleteWord(trie** root, char* str) {
     if (*root == NULL)
         return false;
 
@@ -114,7 +103,7 @@ void deleteWord(trienode** root, char* str) {
 
 
 
-void printTrieMap(trienode* root, int level, char ch) {
+void printTrieMap(trie* root, int level, char ch) {
     if (root == NULL) return;
 
     for (int i = 0; i < level; i++)
@@ -132,7 +121,7 @@ void printTrieMap(trienode* root, int level, char ch) {
     }
 }
 
-void printtrie(trienode* root) {
+void printtrie(trie* root) {
     if (root == NULL) {
         printf("(ROOT) -> EMPTY\n");
         return;
@@ -140,22 +129,122 @@ void printtrie(trienode* root) {
     printTrieMap(root, 0, '\0');
 }
 
+bool searchStr(trie* root, char* str){
+    trie* tmp = root;
+    int len = strlen(str);
+
+    for(int i=0;i<len;i++){
+        if(tmp->children[str[i]]==NULL){
+            return false;
+        }
+        tmp = tmp->children[str[i]];
+    }
+    return tmp->terminal;
+}
 
 
-int main() {
-    trienode* root = NULL;
+void insertWords(trie* currRoot, char* buffer, int depth,
+                 char res[][100], int* ri){
 
+    if(currRoot->terminal){
+        buffer[depth] = '\0';
+        strcpy(res[*ri], buffer);
+        (*ri)++;
+    }
+
+    for(int i=0;i<NUM_CHAR;i++){
+        if(currRoot->children[i]!=NULL){
+            buffer[depth] = (char)i;
+            insertWords(currRoot->children[i], buffer, depth+1, res, ri);
+        }
+    }
+}
+
+void intellisense(char res[][100], char* prefix, trie* root, int* ri){
+
+    trie* tmp = root;
+    int len = strlen(prefix);
+
+    for(int i=0;i<len;i++){
+        if(tmp->children[(unsigned char)prefix[i]]==NULL)
+            return;
+        tmp = tmp->children[(unsigned char)prefix[i]];
+    }
+
+    char buffer[100];
+    strcpy(buffer, prefix);
+    insertWords(tmp, buffer, len, res, ri);
+}
+
+
+char* processChar(char c, bool reset) {
+    static char str[100];
+    static int i = 0;
+
+    if (reset) {
+        i = 0;
+        str[0] = '\0';
+        return str;
+    }
+
+    str[i++] = c;
+    str[i] = '\0';
+    printf("Current input: %s\n", str);
+    return str;
+}
+
+
+
+void main() {
+    char c;
+    char* str;
+    trie* root = NULL;
+    static char res[100][100];
+    int ri = 0;
+
+    // Insert test words
     insertNode(&root,"tea");
     insertNode(&root,"test");
     insertNode(&root,"teacher");
+    insertNode(&root,"team");
+    insertNode(&root,"teach");
 
-    printf("Before Delete:\n");
+    printf("Trie structure:\n");
     printtrie(root);
+    printf("\n");
 
-    deleteWord(&root,"test");
-    printtrie(root);
+    printf("Type characters (press ESC to stop, BACKSPACE to delete):\n");
 
-    return 0;
+    // Reset the static variable in processChar
+    processChar(0, true);
+
+    while(1){
+        c = getch();
+
+        if(c == 27){ // ESC key
+            return;
+        }
+        else if(c == 8) { // BACKSPACE key
+            // Handle backspace - you can implement this if needed
+            continue;
+        }
+        else {
+            str = processChar(c, false);
+            ri = 0;
+            intellisense(res, str, root, &ri);
+
+            printf("Suggestions (%d found): \n", ri);
+            printf("---------------\n");
+            if(ri == 0) {
+                printf("No suggestions found\n");
+            } else {
+                for(int i = 0; i < ri; i++){
+                    printf("%s\n", res[i]);
+                }
+            }
+            printf("---------------\n\n");
+        }
+    }
 }
 
 
